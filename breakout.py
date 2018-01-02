2eb7252bf2406f2d8087252acdd909c7f166e303
marcinic <marcinic@bc.edu>
2018-01-02 16:38:40 -0500

    added target network

--- breakout.py
+++ breakout.py
@@ -1,17 +1,12 @@
 
+import os
 import gym
+from gym import wrappers
 import cv2
 import numpy as np
 from collections import deque
-from replay_buffer import ReplayBuffer
 from dqn import DQN
 
-t = 0
-env = gym.make('Breakout-v0')
-env.reset()
-agent = DQN((84,84),6)
-
-nn = agent.init_model()
 
 
 def max_pixel(prev_frame,frame):
@@ -28,79 +23,135 @@ def extract_luminance(frame):
     Y = cv2.cvtColor(rs,cv2.COLOR_RGB2GRAY)
     return Y
 
+def clip_reward(reward):
+    """Rewards are clipped to be between [-1,1]"""
+    return np.sign(reward)
 
 
-replay = ReplayBuffer(1000000)
+t = 0
+env = gym.make('BreakoutNoFrameskip-v4')
+env.reset()
+moves = env.action_space.n
+#env = wrappers.Monitor(env,'/home/marcinic/hdd/projects/reinforcement/dqn/experiment-1',force=True)
+
+agent = DQN((84,84),moves)
+
+
+if os.path.exists("model/dqn.h5"):
+    print("Loading model")
+    nn = agent.load_model("model/dqn.h5")
+else:
+    nn = agent.init_model()
+
+
 recent_frames = deque(maxlen=4)
-discount_factor = 0.99
-experience = []
-#total_reward = 0.0
-episodes = 100
-num_frames = 10000
+target_update_freq = 10000
+
+episodes = 100000
+num_frames = 10000000000000
 
 i = 0
 for e in range(episodes):
     state = env.reset()
     state = extract_luminance(state)
-    #recent_frames.append(state)
+    #es.append(state)
     reward = 0
     done = False
     action = 0
     time_t = 0
     total_reward = 0
+    state_reward = 0
+    prev_lives = 5
     for time_t in range(num_frames):
 
         #env.render()
         recent_frames.append(state)
-        phi = np.stack(recent_frames,axis=0)
+        phi = np.stack(recent_frames,axis=2)
 
         # initialize with random agent
-        if i < 100000:
+        if i < 50000:
             if i%4==0:
                 action = env.action_space.sample()
             next_state, reward, done, info =  env.step(action)
+            #reward = clip_reward(reward)
+            state_reward+=reward
+
+            #lives = info['ale.lives']
+            #reward = clip_reward(reward)
+            #if lives<prev_lives:
+            #    reward = -10
+            #    prev_lives = lives
+
+
             ns = recent_frames
             state = extract_luminance(next_state)
             ns.append(state)
             next_phi = np.stack(ns,axis=2)
             #next_phi = np.reshape(next_phi,(1,next_phi.shape[0],next_phi.shape[1],next_phi.shape[2]))
             total_reward = total_reward+reward
-            agent.remember(phi,action,total_reward,next_phi,done)
-
+            agent.remember(phi,action,state_reward,next_phi,done)
+            state_reward = 0
             i = i+1
         else:
 
-
+            # Every 4 time steps select a move
             if time_t%4==0:
                 phi = np.stack(recent_frames,axis=2)
                 action = agent.select_arm(phi)
                 next_state, reward, done, info =  env.step(action)
+
+
+                state_reward +=reward
+                 #if lives<prev_lives:
+                #    reward = -10
+                #    prev_lives = lives
+
                 if not done: #np.count_nonzero(next_state)==0:
                     next_state = extract_luminance(next_state)
                     next_state = max_pixel(state,next_state)
                     ns = recent_frames
                     ns.append(next_state)
                     next_phi = np.stack(ns,axis=2)
-                    #replay.add(rf,action,reward,ns,done)
-                    agent.remember(phi,action,reward,next_phi,done)
+
+                    agent.remember(phi,action,state_reward,next_phi,done)
 
                     #agent.update(rf,action,reward,ns,done)
                     state = next_state
 
                 total_reward = total_reward+reward
+                state_reward=0
 
 
 
             else:
                 next_state, reward, done, info =  env.step(action)
+                #reward = clip_reward(reward)
+                state_reward += reward
+            #    if lives<prev_lives:
+            #        reward = -10
+            #        prev_lives = lives
+
                 total_reward = total_reward+reward
 
 
 
 
+        i = i+1
 
         if done:
-            print("episode: {}/{}, score: {}".format(e, episodes,total_reward))
+            print("episode: {}/{}, score: {}, steps {}".format(e, episodes,total_reward,i))
             break
-    if len(agent.memory)>32:
-        agent.replay(32)
+        if(i % target_update_freq == 0):
+            print("Updating target model")
+            agent.update_target_model()
+            agent.replay(agent.batch_size)
+
+    if len(agent.memory)>agent.batch_size:
+            agent.replay(agent.batch_size)
+
+    if e%1000==0:
+        print("Saving model")
+        agent.model.save("model/dqn.h5")
+
+    #if len(agent.memory)>32 and (i % target_update_freq == 0) :
+    #    agent.replay(32)
--- dqn.py
+++ dqn.py
@@ -4,84 +4,81 @@ import random
 import numpy as np
 from collections import deque
 from keras.layers import Input, Conv2D,Dense,Flatten
-from keras.models import Model
-from keras.optimizers import RMSprop
-
+from keras.models import Model, load_model
+from keras.optimizers import RMSprop, Adam
+from keras import backend as K
+from keras.callbacks import TensorBoard
 
 
 
 class DQN():
-    def __init__(self,frame_dims,n_actions,epsilon=.05,discount=.99,hist_len=1000):
+    def __init__(self,frame_dims,n_actions,epsilon=1,discount=.99):
         self.epsilon = epsilon
-        self.min_epsilon = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.1
+        self.final_exploration_frame =1000000
+        self.epsilon_decay = 0.99
         self.learning_rate = 0.00025
         self.discount = discount
         self.frame_dims = frame_dims
         self.n_actions = n_actions
-        self.hist_len = hist_len
-        self.memory = deque(maxlen=1000)
-        self.i = []
-
-
-    def build_model(self):
-        cb = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0,
-          write_graph=True, write_images=True)
-        S = Input(shape=(84,84,4,))
-        h = Conv2D(16, 8, 8, subsample=(4, 4),
-            border_mode='same', activation='relu')(S)
-        h = Conv2D(32, 4, 4, subsample=(2, 2),
-            border_mode='same', activation='relu')(h)
-        h = Flatten()(h)
-        h = Dense(256, activation='relu')(h)
-        V = Dense(self.n_actions,activation='linear')(h)
-        model = Model(S,V)
-        rms = keras.optimizers.RMSprop(lr=0.00025)
-        model.compile(optimizer=rms,loss='mean_squared_error',callbacks=[cb])
-        self.model = model
-
-
+        self.memory = deque(maxlen=10000)
+        self.update_freq = 10000
+        self.batch_size =32
+        self.tb = TensorBoard(log_dir='./logs', write_graph=True,write_images=False)
+        #self.summary_writer = K.summary.FileWriter('./logs/')
 
 
     def init_model(self):
-        cb = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0,
-            write_graph=True, write_images=True)
+        #cb = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0,
+        #    write_graph=True, write_images=True)
         inputs = Input(shape=(84,84,4,))
-        conv_1 = Conv2D(32,kernel_size=(8,8),strides=(4,4),activation="relu",kernel_initializer="glorot_uniform")(inputs)
-        conv_2 = Conv2D(64,kernel_size=(4,4),strides=(2,2),activation="relu",kernel_initializer="glorot_uniform")(conv_1)
-        conv_3 = Conv2D(64,kernel_size=(3,3),strides=(1,1),activation="relu",kernel_initializer="glorot_uniform")(conv_2)
+        conv_1 = Conv2D(32,kernel_size=(8,8),strides=(4,4),activation="relu",kernel_initializer="random_uniform")(inputs)
+        conv_2 = Conv2D(64,kernel_size=(4,4),strides=(2,2),activation="relu",kernel_initializer="random_uniform")(conv_1)
+        conv_3 = Conv2D(64,kernel_size=(3,3),strides=(1,1),activation="relu",kernel_initializer="random_uniform")(conv_2)
         flat = Flatten()(conv_3)
-        dense_1 = Dense(512,activation="relu",kernel_initializer="glorot_uniform")(flat)
-        output = Dense(6)(dense_1)
+        dense_1 = Dense(512,activation="relu",kernel_initializer="random_uniform")(flat)
+        output = Dense(self.n_actions,activation='linear')(flat) #(dense_1)
         model = Model(inputs=inputs,outputs=output)
-        model.compile(optimizer='RMSprop',loss='mean_squared_error',callbacks=[cb])
+        opt = RMSprop(self.learning_rate,clipnorm=1.)
+        #opt = Adam(self.learning_rate)
+        model.compile(optimizer=opt,loss='mse')#,callbacks=[cb])
         self.model = model
+        self.target_model = model
 
+
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def load_model(self,path):
+        self.model = load_model(path)
+        self.target_model = self.model
 
     def select_arm(self,state):
         state = np.reshape(state,(1,84,84,4))
         if random.random() < self.epsilon:
             action = random.randrange(self.n_actions)
         else:
-            #action = values.argmax()
             probabilities = self.model.predict(state)
-            #print(probabilities)
-            self.model.fit(state,probabilities,epochs=1,verbose=0)
-            #print(probabilities.shape)
+            action  = np.argmax(probabilities)
+
+        self.replay(32)
+        return action
+
+    def perceive(self,state):
+        state = np.reshape(state,(1,84,84,4))
+        if random.random() < self.epsilon:
+            action = random.randrange(self.n_actions)
+        else:
+            probabilities = self.model.predict(state)
             action  = np.argmax(probabilities)
         return action
 
-    def update(self,state,action,reward,next_state,done):
-        target = reward
 
-        if not done:
-            target = reward + self.discount * np.amax(self.model.predict(next_state)[0])
-        target_f = self.model.predict(state)
 
-        #loss = (target-np.amax(target_f))**2
-
-        target_f[0][action] = target
-        self.model.fit(state,target_f,epochs=1,verbose=0)
+    def clip_reward(self,reward):
+        """Rewards are clipped to be between [-1,1]"""
+        return np.sign(reward)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward,next_state, done))
@@ -103,11 +100,24 @@ class DQN():
                 T = 0
 
             #print(type(ns))
-            target =  y[2]+ (1-T)*(self.discount * np.amax(self.model.predict( np.reshape(y[3],(1,84,84,4)) )))
-            #print(target)
-            targets.append(target)
+            #print(y[1])
 
-        targets = np.array(targets)
+            Q = self.model.predict(np.reshape(y[0],(1,84,84,4)))
+            #print(Q.shape)
+            future = self.target_model.predict( np.reshape(y[3],(1,84,84,4)) )
+            #print(future)
+            fv = np.amax(future)
+            max_action = np.argmax(future)
+
+
+            #print(max_action==y[1])
+            #print(fv)
+            target =  y[2]+ (1-T)*(self.discount * fv)
+            target = self.clip_reward(target)
+            Q[0,y[1]] = target
+            targets.append(Q)
+
+        targets = np.array(targets).reshape((self.batch_size,self.n_actions))
         #print(targets[0].shape)thon
         return targets
 
@@ -116,28 +126,44 @@ class DQN():
         #print(minibatch[0][1].shape)
         #print(minibatch[0][0])
         states = np.array([state[0] for state in minibatch])
-        actions = np.array([state[1] for state in minibatch])
+        #actions = np.array([state[1] for state in minibatch])
         #print(actions)
         states = states.reshape((batch_size,84,84,4))
         #print(states.shape)
-        next_states = np.array([ns[3] for ns in minibatch])
-        rewards = np.array([r[2] for r in minibatch])
+        #next_states = np.array([ns[3] for ns in minibatch])
+        #rewards = np.array([r[2] for r in minibatch])
 
-        q_values = self.model.predict(states)
-        action_q = np.array([q_value[action] for q_value,action in zip(q_values,actions)]) # Predicted q-value associated with action chosen in replay
-        print(action_q)
-        #print("Q values is "+str(q_values))
+        #q_values = self.model.predict(states)
+
+
+
+        #action_q = np.array([q_value[action] for q_value,action in zip(q_values,actions)]) # Predicted q-value associated with action chosen in replay
+        #print(action_q.shape)
+
         targets = self.make_targets(minibatch)
 
-        print(targets.shape)
+        #avg_q = targets.mean()#q_values.mean()
+        #print("Average Q is "+str(avg_q))
 
-        q_values[]
-        loss = ((targets-action_q)**2).mean()
-        self.model.fit(states,)
-        #weights = self.model.trainable_weights
-        #print(weights)
-        #optimizer = RMSprop(0.00025)
-        #updates = optimizer.get_updates(weights,[],loss)
+        #print(targets)
+        #fit_q = np.insert(q_values,actions,targets,axis=1)
+
+        #for i in range(0,batch_size):
+        #    q_values[i,actions[i]] = targets[i]
+
+        #print(fit_q.shape)
+        #loss = ((targets-action_q)**2).mean()
+
+        #print(self.target_model.get_weights())
+        self.model.fit(states,targets,verbose=0,callbacks=[self.tb]) #q_values)
+
+
+        if self.epsilon>self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+
+
 
     def replay(self, batch_size):
         self.q_learn_minibatch(batch_size)
